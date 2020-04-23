de2de3a5d56591e26e8001e2684aa42d66ef6d4d
Cristian <cristian15nz@gmail.com>
2020-04-23 12:05:33 -0400

    Variables, constantes y tipos de datos primitivos.

---
+++ 01_variables/tipos_datos.js
@@ -0,0 +1,31 @@
+// Texto = String
+let nombreCompleto = "Cristian";
+let apellido = 'Nunez';
+let pais = `Republica Dominicana`;
+
+// Numero = Number
+let edad = 23;
+let year = 2020;
+let millon = 1000000;
+const PI = 3.14;
+let precioDollar = 54.8;
+
+// Logicos = Boolean
+let tieneCarro = false;
+let posseePC = true;
+
+console.log(nombreCompleto);
+console.log(edad);
+console.log(tieneCarro);
+
+console.log(typeof nombreCompleto);
+console.log(typeof edad);
+console.log(typeof tieneCarro);
+
+let precio = 10;
+
+console.log(typeof precio);
+
+let resultado = precio + 15;
+
+console.log(resultado);
---
+++ 01_variables/variables.js
@@ -0,0 +1,45 @@
+// Declarar el precio del dollar
+let precioDollar = 53.2;
+precioDollar * 100;
+
+console.log(precioDollar * 100);
+
+console.log("El precio del dolar fue de " + precioDollar);
+
+console.log(precioDollar * 100);
+
+precioDollar = 54;
+
+console.log(precioDollar * 100);
+
+console.log("El precio actual del dollar es " + precioDollar);
+
+let tipoDeSangre = "AB+";
+
+console.log("Resultados del analisis de acuerdo a tu tipo de sangre " + tipoDeSangre);
+console.log("Resultados del analisis de acuerdo a tu tipo de sangre " + tipoDeSangre);
+
+let nombre = "Cristian";
+const apellido = "Nunez";
+
+console.log(nombre);
+console.log(apellido);
+
+nombre = "Fulano";
+
+console.log(nombre);
+
+// Calcular la circunferencia
+const PI = 3.14;
+let diametro = 10;
+
+console.log( PI * diametro );
+
+diametro = 15;
+
+console.log( PI * diametro );
+
+
+diametro = 1;
+
+console.log( PI * diametro );
